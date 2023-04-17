e46bf26f8e873096c99ddedf6ae7629e3a3fa140
zeal0u5 <122125451+zeal0u5@users.noreply.github.com>
2023-04-17 13:53:07 -0500

    map

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-/*
 let map;
-
 async function initMap() {
   //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
@@ -10,27 +8,4 @@ async function initMap() {
     zoom: 8,
   });
 }
-
 initMap();
-*/
-function initialize() {
-  const fenway = { lat: 42.345573, lng: -71.098326 };
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: fenway,
-    zoom: 14,
-  });
-  const panorama = new google.maps.StreetViewPanorama(
-    document.getElementById("pano"),
-    {
-      position: fenway,
-      pov: {
-        heading: 34,
-        pitch: 10,
-      },
-    }
-  );
-
-  map.setStreetView(panorama);
-}
-
-window.initialize = initialize;
