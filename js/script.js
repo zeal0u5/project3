8a20de7e3949825d9bbf8ab2202717244b726c29
zeal0u5 <122125451+zeal0u5@users.noreply.github.com>
2023-04-16 22:54:19 -0500

    maps?

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
-function initMap() {
+let map;
+
+async function initMap() {
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 39.074300, lng: -76.575743},
     zoom: 8
   });
 }
+initMap();
