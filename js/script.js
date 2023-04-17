6256772a9c475c463a8947c0fdd811c6bc270bfb
zeal0u5 <122125451+zeal0u5@users.noreply.github.com>
2023-04-17 13:26:18 -0500

    maps i hope and swear to god

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*
 let map;
 
 async function initMap() {
@@ -11,3 +12,25 @@ async function initMap() {
 }
 
 initMap();
+*/
+function initialize() {
+  const fenway = { lat: 42.345573, lng: -71.098326 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: fenway,
+    zoom: 14,
+  });
+  const panorama = new google.maps.StreetViewPanorama(
+    document.getElementById("pano"),
+    {
+      position: fenway,
+      pov: {
+        heading: 34,
+        pitch: 10,
+      },
+    }
+  );
+
+  map.setStreetView(panorama);
+}
+
+window.initialize = initialize;
