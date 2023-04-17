c6b64bfd751562ccc525a257d3805e7668da51a1
zeal0u5 <122125451+zeal0u5@users.noreply.github.com>
2023-04-16 23:10:58 -0500

    im gonna cry

--- js/script.js
+++ js/script.js
@@ -4,9 +4,10 @@ async function initMap() {
   //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
 
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 39.074300, lng: -76.575743},
-    zoom: 8
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
   });
 }
+
 initMap();
