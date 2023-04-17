999090a74f0c8b982d330ed553ae79af66012d5b
zeal0u5 <122125451+zeal0u5@users.noreply.github.com>
2023-04-17 14:18:14 -0500

    map? work?

--- js/script.js
+++ js/script.js
@@ -1,11 +1,33 @@
-let map;
-async function initMap() {
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
+// Slider code from https://bxslider.com/
+$(document).ready(function(){
+	$('.slider').bxSlider();
+		
+});
 
-  map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
+var map;
+const iit = {lat:41.835, lng:-87.627};
+const polska = {lat:52, lng:21};
+const goodwill = {lat:41.88284462952575, lng:-87.65705415769303};
+
+function initMap() {
+	map = new google.maps.Map(document.getElementById("map"), {
+		zoom: 15, center:iit,
+	});
+	const marker = new google.maps.Marker({
+		position: iit, map:map,
+	});
 }
-initMap();
+
+function ultraZoom() {
+	map.setZoom(30);
+}
+
+function goToPoland() {
+	map.setCenter(polska);
+	map.setZoom(6);
+}
+
+function goToGoodwill() {
+	map.setCenter(goodwill);
+	map.setZoom(20);
+}
