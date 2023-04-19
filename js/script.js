c981f93ee714722938e6cda38c29a2aede1576a7
zeal0u5 <122125451+zeal0u5@users.noreply.github.com>
2023-04-18 19:00:21 -0500

    added slideshow

--- js/script.js
+++ js/script.js
@@ -23,3 +23,33 @@ function goToSevern() {
 function goToOcean() {
 	map.setCenter(ocean);
 	map.setZoom(15);}
+
+  let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+//this code for the slide show is from w3schools
